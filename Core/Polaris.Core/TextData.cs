aaac742c169586e9ab073d5c592eb3b17663759e
arblic <arblic000110110@gmail.com>
2021-09-06 22:52:35 +0900

    場所移動

--- Core/Polaris.Core/TextData.cs
+++ Core/Polaris.Core/TextData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Polaris.Core {
+﻿namespace Polaris.Core {
 
 	/// <summary>
 	/// テキストデータ
